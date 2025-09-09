0e20afc7afcd8f9c1d0cabf1604900bc4569aead
karmat314 <karmat314@gmail.com>
2025-09-09 14:46:29 +0600

    modified schema

--- db/migrate/20250909084157_create_posts.rb
+++ db/migrate/20250909084157_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[8.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
+      t.text :title
+      t.string :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_09_073902) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_09_084157) do
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.text "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
