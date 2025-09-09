9b4002c89f4f4992c354bbd8e55402c747faca2c
karmat314 <karmat314@gmail.com>
2025-09-09 14:36:07 +0600

    CRUD endpoints for post api

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,40 @@
 class PostsController < ApplicationController
+  def index
+    posts = Post.all
+ response.headers["my-header"] = "DOOMS"
+    render json: posts, status: :found
+  end
+
+  def show
+    post = Post.find_by(id: params[:id])
+    if post
+      render json: {post: post}, status: :found
+    else
+      render json: {message: "Post not found"}, status: :not_found
+    end
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: { message: "Created successfully", post: post}, status: :created
+    else
+      render json: { errors: "Post could not be created"}, status: :unprocessable_entity
+    end
+  end
+
+  
+  def destroy
+    post = Post.find_by(id: params[:id]) 
+    if post
+      post.destroy
+      render json: { message: "Post deleted successfully" }, status: :ok
+    else
+      render json: { error: "Post not found" }, status: :not_found
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+## Create sample posts
+Post.destroy_all
+posts = [
+  { title: "First Post", body: "This is the body of the first post." },
+  { title: "Second Post", body: "Here’s some more content for the second post." },
+  { title: "Final Post", body: "This is just a demo seed to populate the database with some posts." }
+]
+
+posts.each do |post|
+  Post.create!(post)
+end
+
